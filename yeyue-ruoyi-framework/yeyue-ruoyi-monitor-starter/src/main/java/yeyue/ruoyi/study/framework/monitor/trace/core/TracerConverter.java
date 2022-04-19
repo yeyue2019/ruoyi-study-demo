7d108223abb0f51edf8d23c7498655d28b948999
yeyue <yeyue2019@aliyun.com>
2022-04-19 23:02:15 +0800

    trace生效的提交

---
+++ yeyue-ruoyi-framework/yeyue-ruoyi-monitor-starter/src/main/java/yeyue/ruoyi/study/framework/monitor/trace/core/TracerConverter.java
@@ -0,0 +1,37 @@
+package yeyue.ruoyi.study.framework.monitor.trace.core;
+
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.pattern.*;
+
+/**
+ * traceId追踪插件
+ *
+ * @author fred
+ * @date 2021-09-30 15:12
+ */
+@Plugin(name = "TracerConverter", category = PatternConverter.CATEGORY)
+@ConverterKeys({"trace"})
+public class TracerConverter extends LogEventPatternConverter {
+
+    /**
+     * Constructs an instance of LoggingEventPatternConverter.
+     *
+     * @param name  name of converter.
+     * @param style CSS style for output.
+     */
+    protected TracerConverter(String name, String style) {
+        super(name, style);
+    }
+
+    public static TracerConverter newInstance(String[] options) {
+        return new TracerConverter("trace", "trace");
+    }
+
+    @Override
+    public void format(LogEvent event, StringBuilder toAppendTo) {
+        toAppendTo.append("AAA");
+    }
+}
+
